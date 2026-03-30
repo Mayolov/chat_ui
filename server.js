a539cd79cce32ecd71649e119e95d60b9c1c704c
mayolov <mayolo@TensorStation>
2026-03-30 15:43:13 -0700

    SSL cert issue fix

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Allow self-signed / internal CA certificates (e.g. enterprise proxies)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
